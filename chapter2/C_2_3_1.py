1ac78c1a0dc74a87803d316e69b2d9f773b2f64e
Jace Liu <ly950710@gmail.com>
2017-01-18 18:27:38 +0800

    Content 2.3-1 Merge Sort
    Signed-off-by: Jace Liu <ly950710@gmail.com>

---
+++ chapter2/C_2_3_1.py
@@ -0,0 +1,37 @@
+# Divide and Conquer
+# Merge Sort
+
+def merge_sort(arr):
+    length = len(arr)
+    if length == 1:
+        return arr
+    else:
+        print(arr)
+        left = merge_sort(arr[:length//2])
+        right = merge_sort(arr[length//2:])
+        result = []
+        i = 0
+        j = 0
+        len_left = len(left)
+        len_right = len(right)
+        while i < len_left and j < len_right:
+            if left[i] > right[j]:
+                result.append(right[j])
+                j += 1
+            else:
+                result.append(left[i])
+                i += 1
+        if i == len_left:
+            while j < len_right:
+                result.append(right[j])
+                j += 1
+        else:
+            while i < len_left:
+                result.append(left[i])
+                i += 1
+        return result
+
+def main():
+    print(merge_sort([3,4,5,2,1,4,6,4,6,2,6,7,8,5,2,12,31,52,5]))
+
+main()
