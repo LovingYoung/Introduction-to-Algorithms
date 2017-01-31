6345559e3bd73472ede5c30675a7a74d9d1639f4
Jace Liu <ly950710@gmail.com>
2017-01-31 18:12:14 +0800

    Chapter 8: Sorting in linear time -- Bucket sort
    Signed-off-by: Jace Liu <ly950710@gmail.com>

--- chapter8/LinearTimeSort.h
+++ chapter8/LinearTimeSort.h
@@ -31,4 +31,85 @@ int * countsort(int * array, int n, int k){
     return result;
 }
 
+struct ListNode{
+    double val;
+    ListNode * next;
+
+    ListNode(){
+        next = NULL;
+    }
+
+    ListNode(double value): val(value), next(NULL){};
+};
+
+void addElement(ListNode* &node, double value){
+    if(node == NULL){
+        node = new ListNode(value);
+    } else addElement(node->next, value);
+}
+
+ListNode * quicksort(ListNode * start){
+    if(start == NULL) return NULL;
+    ListNode * left = NULL;
+    ListNode * left_end = NULL;
+    ListNode * right = NULL;
+    ListNode * right_end = NULL;
+    double key = start->val;
+    ListNode * p = start->next;
+    while(p != NULL){
+        ListNode * current = p;
+        p = p->next;
+        current->next = NULL;
+        if(current->val<= key){
+            if(left_end == NULL){
+                left_end = left = current;
+            } else {
+                left_end->next = current;
+                left_end = current;
+            }
+        } else {
+            if(right_end == NULL){
+                right_end = right = current;
+            } else {
+                right_end->next = current;
+                right_end = current;
+            }
+        }
+    }
+    if(left != NULL) left = quicksort(left);
+    if(right != NULL) right = quicksort(right);
+    ListNode * result;
+    if(left != NULL){
+        result = left;
+        while(left -> next != NULL) left = left->next;
+        left->next = start;
+    } else {
+        result = start;
+    }
+    start->next = right;
+    return result;
+}
+
+double * bucketsort(double * array, int n){
+    ListNode* temp[n];
+    memset(temp, NULL, n * sizeof(ListNode*));
+    for(int i = 0; i < n; i++){
+        int loc = n * array[i];
+        addElement(temp[loc], array[i]);
+    }
+    for(int i = 0; i < n; i++){
+        temp[i] = quicksort(temp[i]);
+    }
+    double * result = (double*) malloc (sizeof(double) * n);
+    int l = 0;
+    for(int i = 0; i < n; i++){
+        while(temp[i] != NULL){
+            result[l] = temp[i]->val;
+            l += 1;
+            temp[i] = temp[i] -> next;
+        }
+    }
+    return result;
+}
+
 #endif
--- chapter8/main.cpp
+++ chapter8/main.cpp
@@ -10,8 +10,18 @@ void print(int * array, int n){
     cout << endl;
 }
 
+void print(double * array, int n){
+    for(int i = 0; i < n; i++){
+        cout << array[i] << ' ';
+    }
+    cout << endl;
+}
+
 int main(){
     int countArray[10] = {19, 14, 2, 8, 1, 7, 19, 16, 3, 20};
     int * result = countsort((int*) countArray, 10, 20);
     print(result, 10);
+    double bucketArray[10] = {0.78, 0.17, 0.39, 0.26, 0.72, 0.94, 0.21, 0.12, 0.23, 0.68};
+    double * result2 = bucketsort((double*) bucketArray, 10); 
+    print(result2, 10);
 }
